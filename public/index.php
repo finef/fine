bfb832f48406d8f0e9c2b52486391661bd869380
finef <finephpframework@gmail.com>
2016-05-27 23:13:11 +0200

    update

--- public/index.php
+++ public/index.php
@@ -7,4 +7,4 @@ chdir(dirname(__DIR__));
 require_once __DIR__ . '/vendor/autoload.php';
 
 // bootstrap application
-\Fine\Application\Application::setInstance(new \Fine\Application\Application())->bootstrap(require 'config/module.php');
+(new \Fine\Application\Application())->bootstrap(require 'config/module.php');
